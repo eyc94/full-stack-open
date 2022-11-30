9646ab7a2ca0ff10dd12a2fee349528d5be0cf97
Eric Chin <ericychin94@gmail.com>
2022-11-29 23:22:41 -0800

    Exercise 1.6

---
+++ 01. Introduction To React/unicafe/src/App.js
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+const App = () => {
+  // Save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGood = () => {
+    setGood(good + 1);
+  };
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const handleBad = () => {
+    setBad(bad + 1);
+  };
+
+  return (
+    <div>
+      <h2>Give Feedback</h2>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
+
+      <h2>Statistics</h2>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+    </div>
+  );
+};
+
+export default App;
