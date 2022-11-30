1c913aa176887137b3d939df876956a4a02657d9
Eric Chin <ericychin94@gmail.com>
2022-11-29 16:56:01 -0800

    Stateful component

--- 01. Introduction To React/part1c/src/index.js
+++ 01. Introduction To React/part1c/src/index.js
@@ -2,14 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-let counter = 1;
-
-const refresh = () => {
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(<App counter={counter} />);
-};
-
-setInterval(() => {
-  refresh();
-  counter += 1;
-}, 1000);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
