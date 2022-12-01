f3e40865587485f244cb773476cbc80e94cebe88
Eric Chin <ericychin94@gmail.com>
2022-11-30 17:46:48 -0800

    Effect-hooks

--- 02. Communicating With Server/notes-frontend/src/index.js
+++ 02. Communicating With Server/notes-frontend/src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import axios from 'axios';
 
 import App from './App';
 
-axios.get('http://localhost:3001/notes').then(response => {
-  const notes = response.data;
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(<App notes={notes} />);
-});
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
