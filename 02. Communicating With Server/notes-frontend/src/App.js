23c4c4cfa0acb3e2389d9e56ff93e3b981702e9d
Eric Chin <ericychin94@gmail.com>
2022-12-01 01:45:43 -0800

    Improved error message

--- 02. Communicating With Server/notes-frontend/src/App.js
+++ 02. Communicating With Server/notes-frontend/src/App.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react';
 import Note from './components/Note';
+import Notification from './components/Notification';
 import noteService from './services/notes';
 
 const App = (props) => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('');
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     noteService
@@ -25,7 +27,10 @@ const App = (props) => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote));
       })
       .catch(error => {
-        alert(`The note ${note.content} was already deleted from the server`);
+        setErrorMessage(`The note ${note.content} was already deleted from the server`);
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
         setNotes(notes.filter(n => n.id !== id));
       })
   };
@@ -57,6 +62,7 @@ const App = (props) => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
