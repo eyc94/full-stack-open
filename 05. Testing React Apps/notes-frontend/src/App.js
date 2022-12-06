516d0a1a76de9d1c2013ca2632a52cb30e39354c
Eric Chin <ericychin94@gmail.com>
2022-12-06 00:41:14 -0800

    Creating new notes

--- 05. Testing React Apps/notes-frontend/src/App.js
+++ 05. Testing React Apps/notes-frontend/src/App.js
@@ -71,6 +71,8 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       });
+
+      noteService.setToken(user.token);
       setUser(user);
       setUsername('');
       setPassword('');
