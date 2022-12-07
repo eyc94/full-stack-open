ecd8b37986dd48c8524532a3e42ecb250d6fcb4b
Eric Chin <ericychin94@gmail.com>
2022-12-07 10:15:41 -0800

    Writing to a form

--- 05. Testing React Apps/notes-frontend/cypress/e2e/note_app.cy.js
+++ 05. Testing React Apps/notes-frontend/cypress/e2e/note_app.cy.js
@@ -1,7 +1,23 @@
 describe('Note app', function() {
-  it('front page can be opened', function() {
+  beforeEach(function() {
     cy.visit('http://localhost:3000');
+  });
+
+  it('front page can be opened', function() {
     cy.contains('Notes');
     cy.contains('Note App, EC 2022');
+  });
+
+  it('login form can be opened', function() {
+    cy.contains('Login').click();
+  });
+
+  it('user can login', function() {
+    cy.contains('Login').click();
+    cy.get('#username').type('echin');
+    cy.get('#password').type('password');
+    cy.get('#login-button').click();
+
+    cy.contains('Eric logged-in');
   });
 });
