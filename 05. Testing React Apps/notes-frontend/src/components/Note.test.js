c019385041ee695c17d7b329289ce6a07c2b1a3d
Eric Chin <ericychin94@gmail.com>
2022-12-07 02:21:14 -0800

    Searching for content in a component

--- 05. Testing React Apps/notes-frontend/src/components/Note.test.js
+++ 05. Testing React Apps/notes-frontend/src/components/Note.test.js
@@ -11,6 +11,13 @@ test('renders content', () => {
 
   render(<Note note={note} />);
 
-  const element = screen.getByText('Component testing is done with react-testing-library');
-  expect(element).toBeDefined();
+  // const element = screen.getByText('Component testing is done with react-testing-library');
+  // expect(element).toBeDefined();
+
+  const { container } = render(<Note note={note} />);
+
+  const div = container.querySelector('.note');
+  expect(div).toHaveTextContent(
+    'Component testing is done with react-testing-library'
+  );
 });
