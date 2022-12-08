dddbd52d6f1f3f114a22fdca9b75bc5e911999fc
Eric Chin <ericychin94@gmail.com>
2022-12-08 00:54:03 -0800

    Exercise 6.6

--- 06. State Management With Redux/anecdotes-redux/src/reducers/anecdoteReducer.js
+++ 06. State Management With Redux/anecdotes-redux/src/reducers/anecdoteReducer.js
@@ -19,6 +19,20 @@ const asObject = (anecdote) => {
 
 const initialState = anecdotesAtStart.map(asObject);
 
+export const createAnecdote = (content) => {
+  return {
+    type: 'NEW_ANECDOTE',
+    data: content,
+  };
+};
+
+export const voteAction = (id) => {
+  return {
+    type: 'VOTE',
+    data: { id },
+  };
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'NEW_ANECDOTE':
