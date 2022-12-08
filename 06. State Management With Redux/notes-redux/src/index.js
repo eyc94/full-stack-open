bef36a96142afa89a7b98fc2cd800bac0518bab9
Eric Chin <ericychin94@gmail.com>
2022-12-07 22:41:10 -0800

    Uncontrolled form

--- 06. State Management With Redux/notes-redux/src/index.js
+++ 06. State Management With Redux/notes-redux/src/index.js
@@ -24,12 +24,43 @@ store.dispatch({
   },
 });
 
+const generateId = () => {
+  return Number((Math.random() * 1000000).toFixed(0));
+};
+
 const App = () => {
+  const addNote = (event) => {
+    event.preventDefault();
+    const content = event.target.note.value;
+    event.target.note.value = '';
+    store.dispatch({
+      type: 'NEW_NOTE',
+      data: {
+        content,
+        important: false,
+        id: generateId(),
+      },
+    });
+  };
+
+  const toggleImportance = (id) => {
+    store.dispatch({
+      type: 'TOGGLE_IMPORTANCE',
+      data: {
+        id,
+      },
+    });
+  };
+
   return (
     <div>
+      <form onSubmit={addNote}>
+        <input name='note' />
+        <button type='submit'>add</button>
+      </form>
       <ul>
         {store.getState().map(note =>
-          <li key={note.id}>
+          <li key={note.id} onClick={() => toggleImportance(note.id)}>
             {note.content} <strong>{note.important ? 'important' : ''}</strong>
           </li>
         )}
@@ -39,4 +70,7 @@ const App = () => {
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const renderApp = () => root.render(<App />);
+
+renderApp();
+store.subscribe(renderApp);
--- 06. State Management With Redux/notes-redux/src/reducers/noteReducer.js
+++ 06. State Management With Redux/notes-redux/src/reducers/noteReducer.js
@@ -2,7 +2,7 @@ const noteReducer = (state = [], action) => {
   switch(action.type) {
     case 'NEW_NOTE':
       return state.concat(action.data);
-    case 'TOGGLE_IMPORTANCE':
+    case 'TOGGLE_IMPORTANCE': {
       const id = action.data.id;
       const noteToChange = state.find(n => n.id === id);
       const changedNote = {
@@ -12,6 +12,7 @@ const noteReducer = (state = [], action) => {
       return state.map(note =>
         note.id !== id ? note : changedNote
       );
+    }
     default:
       return state;
   };
