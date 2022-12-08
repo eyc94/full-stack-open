47234df8834a28098d912c39f882903e2d98f0be
Eric Chin <ericychin94@gmail.com>
2022-12-08 00:59:12 -0800

    Exercise 6.8

--- 06. State Management With Redux/anecdotes-redux/src/App.js
+++ 06. State Management With Redux/anecdotes-redux/src/App.js
@@ -1,29 +1,11 @@
-import { useSelector, useDispatch } from 'react-redux';
 import AnecdoteForm from './components/AnecdoteForm';
-import { voteAction } from './reducers/anecdoteReducer';
+import AnecdoteList from './components/AnecdoteList';
 
 const App = () => {
-  const anecdotes = useSelector(state => state);
-  const dispatch = useDispatch();
-
-  const vote = (id) => {
-    dispatch(voteAction(id));
-  };
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>Vote</button>
-          </div>
-        </div>  
-      )}
+      <AnecdoteList />
       <AnecdoteForm />
     </div>
   );
