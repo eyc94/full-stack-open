03171a42152e94eb04e8161f6c899dbc236374f3
Eric Chin <ericychin94@gmail.com>
2022-12-08 01:28:53 -0800

    Combined reducers

--- 06. State Management With Redux/notes-redux/src/index.js
+++ 06. State Management With Redux/notes-redux/src/index.js
@@ -3,15 +3,28 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux'
-import noteReducer from './reducers/noteReducer';
+import noteReducer, { createNote } from './reducers/noteReducer';
+import filterReducer, { filterChange } from './reducers/filterReducer';
 
-const store = createStore(noteReducer);
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer,
+});
+
+const store = createStore(reducer);
+
+console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  // <Provider store={store}>
+  //   <App />
+  // </Provider>
+  <div />
 );
+
+store.subscribe(() => console.log(store.getState()));
+store.dispatch(filterChange('IMPORTANT'));
+store.dispatch(createNote('combinedReducers forms one reducer from many simple reducers'));
