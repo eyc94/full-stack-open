c6c039cc67b9004fb637f087283bd3604ecc14ed
Eric Chin <ericychin94@gmail.com>
2022-12-07 23:25:24 -0800

    Exercise template

---
+++ 06. State Management With Redux/anecdotes-redux/src/App.js
@@ -0,0 +1,34 @@
+import { useSelector, useDispatch } from 'react-redux';
+
+const App = () => {
+  const anecdotes = useSelector(state => state);
+  const dispatch = useDispatch();
+
+  const vote = (id) => {
+    console.log('vote', id);
+  };
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>Vote</button>
+          </div>
+        </div>  
+      )}
+      <h2>Create New</h2>
+      <form>
+        <div><input /></div>
+        <button>Create</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
---
+++ 06. State Management With Redux/anecdotes-redux/src/components/Notification.js
@@ -0,0 +1,15 @@
+const Notification = () => {
+  const style = {
+    border: 'style',
+    padding: 10,
+    borderWidth: 1,
+  };
+
+  return (
+    <div style={style}>
+      render notification here...
+    </div>
+  );
+};
+
+export default Notification;
