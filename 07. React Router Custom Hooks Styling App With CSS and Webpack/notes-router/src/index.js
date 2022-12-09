14846bfe82b19eb2d41294ba1f255c619dd0e972
Eric Chin <ericychin94@gmail.com>
2022-12-08 23:24:31 -0800

    React Router

--- 07. React Router Custom Hooks Styling App With CSS and Webpack/notes-router/src/index.js
+++ 07. React Router Custom Hooks Styling App With CSS and Webpack/notes-router/src/index.js
@@ -1,5 +1,8 @@
-import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from 'react-router-dom';
 
 const Home = () => (
   <div><h2>Notes App</h2></div>
@@ -14,42 +17,27 @@ const Users = () => (
 );
 
 const App = () => {
-  const [page, setPage] = useState('home');
-
-  const toPage = (page) => (event) => {
-    event.preventDefault();
-    setPage(page);
-  };
-
-  const content = () => {
-    if (page === 'home') {
-      return <Home />
-    } else if (page === 'notes') {
-      return <Notes />
-    } else if (page === 'users') {
-      return <Users />
-    }
-  };
-
   const padding = {
     padding: 5,
   };
 
   return (
-    <div>
+    <Router>
       <div>
-        <a href='' onClick={toPage('home')} style={padding}>
-          home
-        </a>
-        <a href='' onClick={toPage('notes')} style={padding}>
-          notes
-        </a>
-        <a href='' onClick={toPage('users')} style={padding}>
-          users
-        </a>
+        <Link style={padding} to='/'>Home</Link>
+        <Link style={padding} to='/notes'>Notes</Link>
+        <Link style={padding} to='/users'>Users</Link>
       </div>
-      {content()}
-    </div>
+      <Routes>
+        <Route path='/notes' element={<Notes />} />
+        <Route path='/users' element={<Users />} />
+        <Route path='/' element={<Home />} />
+      </Routes>
+
+      <div>
+        <i>Note App EC 2022</i>
+      </div>
+    </Router>
   );
 };
 
