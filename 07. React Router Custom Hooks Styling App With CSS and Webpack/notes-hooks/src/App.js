f0eb35894a614881b03208a4d48f31c4b1b32e9a
Eric Chin <ericychin94@gmail.com>
2022-12-09 04:10:54 -0800

    Custom hooks, step 2

--- 07. React Router Custom Hooks Styling App With CSS and Webpack/notes-hooks/src/App.js
+++ 07. React Router Custom Hooks Styling App With CSS and Webpack/notes-hooks/src/App.js
@@ -24,20 +24,19 @@ const useCounter = () => {
 };
 
 const App = () => {
-  const counter = useCounter();
+  const left = useCounter();
+  const right = useCounter();
 
   return (
     <div>
-      <div>{counter.value}</div>
-      <button onClick={counter.increase}>
-        plus
+      {left.value}
+      <button onClick={left.increase}>
+        left
+      </button>  
+      <button onClick={right.increase}>
+        right
       </button>
-      <button onClick={counter.decrease}>
-        minus
-      </button>      
-      <button onClick={counter.zero}>
-        zero
-      </button>
+      {right.value}
     </div>
   );
 };
