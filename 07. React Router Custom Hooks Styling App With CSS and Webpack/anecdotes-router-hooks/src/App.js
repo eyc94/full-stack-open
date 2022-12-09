d236b1b0b1603a105b0dcdebb603b74f859ed447
Eric Chin <ericychin94@gmail.com>
2022-12-09 04:28:11 -0800

    Exercise 7.4

--- 07. React Router Custom Hooks Styling App With CSS and Webpack/anecdotes-router-hooks/src/App.js
+++ 07. React Router Custom Hooks Styling App With CSS and Webpack/anecdotes-router-hooks/src/App.js
@@ -7,6 +7,7 @@ import {
   useParams,
   useNavigate,
 } from 'react-router-dom';
+import { useField } from './hooks';
 
 const Menu = () => {
   const padding = {
@@ -72,21 +73,18 @@ const Footer = () => (
 const CreateNew = (props) => {
   const navigate = useNavigate();
 
-  const [content, setContent] = useState('');
-  const [author, setAuthor] = useState('');
-  const [info, setInfo] = useState('');
+  const content = useField('text');
+  const author = useField('text');
+  const info = useField('text');
 
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     });
-    setContent('');
-    setAuthor('');
-    setInfo('');
     navigate('/');
   };
 
@@ -96,15 +94,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           Content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input { ...content } />
         </div>
         <div>
           Author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input { ...author } />
         </div>
         <div>
           URL for more info
-          <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+          <input { ...info } />
         </div>
         <button>Create</button>
       </form>
