ed54cf3c6454e9d0784480be21842de29db2135f
Eric Chin <ericychin94@gmail.com>
2022-12-09 04:15:53 -0800

    Custom hooks, step 3

--- 07. React Router Custom Hooks Styling App With CSS and Webpack/notes-hooks/src/App.js
+++ 07. React Router Custom Hooks Styling App With CSS and Webpack/notes-hooks/src/App.js
@@ -1,42 +1,51 @@
 import { useState } from 'react';
 
-const useCounter = () => {
-  const [value, setValue] = useState(0);
+const useField = (type) => {
+  const [value, setValue] = useState('');
 
-  const increase = () => {
-    setValue(value + 1);
-  };
-
-  const decrease = () => {
-    setValue(value - 1);
-  };
-
-  const zero = () => {
-    setValue(0);
+  const onChange = (event) => {
+    setValue(event.target.value);
   };
 
   return {
+    type,
     value,
-    increase,
-    decrease,
-    zero,
+    onChange,
   };
 };
 
 const App = () => {
-  const left = useCounter();
-  const right = useCounter();
+  const name = useField('text');
+  const born = useField('date');
+  const height = useField('number');
 
   return (
     <div>
-      {left.value}
-      <button onClick={left.increase}>
-        left
-      </button>  
-      <button onClick={right.increase}>
-        right
-      </button>
-      {right.value}
+      <form>
+        name: 
+        <input
+          type={name.type}
+          value={name.value}
+          onChange={name.onChange} 
+        /> 
+        <br/> 
+        birthdate:
+        <input
+          type={born.type}
+          value={born.value}
+          onChange={born.onChange}
+        />
+        <br /> 
+        height:
+        <input
+          type={height.type}
+          value={height.value}
+          onChange={height.onChange}
+        />
+      </form>
+      <div>
+        {name.value} {born.value} {height.value} 
+      </div>
     </div>
   );
 };
