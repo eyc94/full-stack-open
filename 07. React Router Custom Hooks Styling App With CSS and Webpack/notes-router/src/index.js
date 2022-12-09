a6d299156495c84a35e5183c60f326e2bb99408e
Eric Chin <ericychin94@gmail.com>
2022-12-08 23:55:57 -0800

    Parameterized route revisited

--- 07. React Router Custom Hooks Styling App With CSS and Webpack/notes-router/src/index.js
+++ 07. React Router Custom Hooks Styling App With CSS and Webpack/notes-router/src/index.js
@@ -8,6 +8,7 @@ import {
   Navigate,
   useParams,
   useNavigate,
+  useMatch,
 } from 'react-router-dom';
 
 const Home = () => (
@@ -17,9 +18,7 @@ const Home = () => (
   </div>
 );
 
-const Note = ({ notes }) => {
-  const id = useParams().id;
-  const note = notes.find(n => n.id === Number(id));
+const Note = ({ note }) => {
   return (
     <div>
       <h2>{note.content}</h2>
@@ -110,27 +109,30 @@ const App = () => {
     padding: 5,
   };
 
+  const match = useMatch('/notes/:id');
+  const note = match
+    ? notes.find(note => note.id === Number(match.params.id))
+    : null;
+
   return (
     <div>
-      <Router>
-        <div>
-          <Link style={padding} to='/'>Home</Link>
-          <Link style={padding} to='/notes'>Notes</Link>
-          <Link style={padding} to='/users'>Users</Link>
-          {user
-            ? <em>{user} logged in</em>
-            : <Link style={padding} to='/login'>Login</Link>
-          }
-        </div>
+      <div>
+        <Link style={padding} to='/'>Home</Link>
+        <Link style={padding} to='/notes'>Notes</Link>
+        <Link style={padding} to='/users'>Users</Link>
+        {user
+          ? <em>{user} logged in</em>
+          : <Link style={padding} to='/login'>Login</Link>
+        }
+      </div>
 
-        <Routes>
-          <Route path='/notes/:id' element={<Note notes={notes} />} />
-          <Route path='/notes' element={<Notes notes={notes} />} />
-          <Route path='/users' element={user ? <Users /> : <Navigate replace to='/login' />} />
-          <Route path='/login' element={<Login onLogin={login} />} />
-          <Route path='/' element={<Home />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path='/notes/:id' element={<Note note={note} />} />
+        <Route path='/notes' element={<Notes notes={notes} />} />
+        <Route path='/users' element={user ? <Users /> : <Navigate replace to='/login' />} />
+        <Route path='/login' element={<Login onLogin={login} />} />
+        <Route path='/' element={<Home />} />
+      </Routes>
       <footer>
         <br />
         <em>Note App EC 2022</em>
@@ -139,4 +141,8 @@ const App = () => {
   );
 };
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Router>
+    <App />
+  </Router>
+);
