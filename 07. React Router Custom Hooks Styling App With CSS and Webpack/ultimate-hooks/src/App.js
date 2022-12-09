93f2e9b3d5cab5fa4f34256cb07cdf9fc9474194
Eric Chin <ericychin94@gmail.com>
2022-12-09 14:47:08 -0800

    Exercise 7.8

--- 07. React Router Custom Hooks Styling App With CSS and Webpack/ultimate-hooks/src/App.js
+++ 07. React Router Custom Hooks Styling App With CSS and Webpack/ultimate-hooks/src/App.js
@@ -8,20 +8,44 @@ const useField = (type) => {
     setValue(event.target.value);
   };
 
+  const reset = () => {
+    setValue('');
+  };
+
   return {
     type,
     value,
-    onChange
+    onChange,
+    reset,
   };
 };
 
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([]);
 
-  // ...
+  let token = null;
 
-  const create = (resource) => {
-    // ...
+  const setToken = newToken => {
+    token = `bearer ${newToken}`;
+  };
+
+  const getAll = async () => {
+    const response = await axios.get(baseUrl);
+    setResources(response.data);
+  };
+
+  useEffect(() => {
+    getAll();
+  }, []);
+
+  const create = async (resource) => {
+    const config = {
+      headers: { Authorization: token },
+    };
+  
+    const response = await axios.post(baseUrl, resource, config);
+    setResources(resources.concat(response.data));
+    return response.data;
   };
 
   const service = {
@@ -29,14 +53,14 @@ const useResource = (baseUrl) => {
   };
 
   return [
-    resources, service
+    resources, service, setToken,
   ];
 };
 
 const App = () => {
-  const content = useField('text');
-  const name = useField('text');
-  const number = useField('text');
+  const { reset: resetContent, ...content } = useField("text");
+  const { reset: resetName, ...name } = useField("text");
+  const { reset: resetNumber, ...number } = useField("text");
 
   const [notes, noteService] = useResource('http://localhost:3001/notes');
   const [persons, personService] = useResource('http://localhost:3001/persons');
@@ -44,11 +68,14 @@ const App = () => {
   const handleNoteSubmit = (event) => {
     event.preventDefault();
     noteService.create({ content: content.value });
+    resetContent();
   };
  
   const handlePersonSubmit = (event) => {
     event.preventDefault();
     personService.create({ name: name.value, number: number.value});
+    resetName();
+    resetNumber();
   };
 
   return (
