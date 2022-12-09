f11e4d2c4a7cfead81a60e73126a31a4ecae7e45
Eric Chin <ericychin94@gmail.com>
2022-12-09 14:25:19 -0800

    Exercise 7.7

--- 07. React Router Custom Hooks Styling App With CSS and Webpack/country-hooks/src/App.js
+++ 07. React Router Custom Hooks Styling App With CSS and Webpack/country-hooks/src/App.js
@@ -18,7 +18,19 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null);
 
-  useEffect(() => {});
+  useEffect(() => {
+    if (name) {
+      axios
+      .get(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
+      .then(response => {
+        console.log(response.data[0]);
+        setCountry(response.data[0]);
+      })
+      .catch(() => {
+        setCountry(null);
+      });
+    }
+  }, [name]);
 
   return country;
 };
