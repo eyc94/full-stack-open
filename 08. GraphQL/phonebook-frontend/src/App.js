cff94ec001e0def19ec1b45161b33525feabf6ae
Eric Chin <ericychin94@gmail.com>
2022-12-10 13:46:03 -0800

    Handling mutation errors

--- 08. GraphQL/phonebook-frontend/src/App.js
+++ 08. GraphQL/phonebook-frontend/src/App.js
@@ -1,23 +1,41 @@
 import { useQuery } from '@apollo/client';
+import { useState } from 'react';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
 import { ALL_PERSONS } from './queries';
 
 const App = () => {
-  // --- Fetch ALL_PERSONS every 2 seconds ---
-  // const result = useQuery(ALL_PERSONS, {
-  //   pollInterval: 2000,
-  // });
+  const [errorMessage, setErrorMessage] = useState(null);
   const result = useQuery(ALL_PERSONS);
 
   if (result.loading) {
     return <div>loading...</div> 
   }
 
+  const notify = (message) => {
+    setErrorMessage(message);
+    setTimeout(() => {
+      setErrorMessage(null);
+    }, 10000);
+  };
+
   return (
     <div>
+      <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons} />
-      <PersonForm />
+      <PersonForm setError={notify} />
+    </div>
+  );
+};
+
+const Notify = ({ errorMessage }) => {
+  if (!errorMessage) {
+    return null;
+  }
+
+  return (
+    <div style={{ color: 'red' }}>
+      {errorMessage}
     </div>
   );
 };
--- 08. GraphQL/phonebook-frontend/src/components/PersonForm.js
+++ 08. GraphQL/phonebook-frontend/src/components/PersonForm.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { CREATE_PERSON, ALL_PERSONS } from '../queries';
 
-const PersonForm = (props) => {
+const PersonForm = ({ setError }) => {
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('');
   const [street, setStreet] = useState('');
@@ -10,6 +10,9 @@ const PersonForm = (props) => {
 
   const [ createPerson ] = useMutation(CREATE_PERSON, {
     refetchQueries: [ { query: ALL_PERSONS } ],
+    onError: (error) => {
+      setError(error.graphQLErrors[0].message);
+    },
   });
 
   const submit = (event) => {
