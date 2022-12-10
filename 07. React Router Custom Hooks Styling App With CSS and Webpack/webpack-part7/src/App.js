30d7a66d804dee66e2768c0dadd6e5c03392a553
Eric Chin <ericychin94@gmail.com>
2022-12-09 17:10:53 -0800

    Webpack bundling, Configuration file, Bundling React, Loaders, Transpilers, CSS, Webpack-dev-server

---
+++ 07. React Router Custom Hooks Styling App With CSS and Webpack/webpack-part7/src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import './index.css';
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div className='container'>
+      Hello webpack {counter} clicks
+      <button onClick={() => setCounter(counter + 1)}>
+        Press
+      </button>
+    </div>
+  );
+};
+
+export default App;
