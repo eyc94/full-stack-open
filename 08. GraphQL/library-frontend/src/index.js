59c8cc81ed3dbbfa486b6d3098103ba333a0b9f1
Eric Chin <ericychin94@gmail.com>
2022-12-10 14:44:00 -0800

    Exercise 8.8

--- 08. GraphQL/library-frontend/src/index.js
+++ 08. GraphQL/library-frontend/src/index.js
@@ -2,7 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from '@apollo/client';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4000',
+  }),
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <ApolloProvider client={client} >
+    <App />
+  </ApolloProvider>
 );
