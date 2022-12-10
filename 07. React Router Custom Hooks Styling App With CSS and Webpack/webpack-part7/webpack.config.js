53e1096a74af6aaf96e25bc8e9ad90ea24ccfff0
Eric Chin <ericychin94@gmail.com>
2022-12-09 17:33:20 -0800

    Development and production configuration

--- 07. React Router Custom Hooks Styling App With CSS and Webpack/webpack-part7/webpack.config.js
+++ 07. React Router Custom Hooks Styling App With CSS and Webpack/webpack-part7/webpack.config.js
@@ -1,39 +1,53 @@
 const path = require('path');
+const webpack = require('webpack');
 
-const config = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'main.js',
-  },
-  devServer: {
-    static: path.resolve(__dirname, 'build'),
-    compress: true,
-    port: 3000,
-  },
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
+const config = (env, argv) => {
+  console.log('argv', argv.mode);
+
+  const backend_url = argv.mode === 'production'
+    ? 'https://ec-notes-backend.fly.dev/api/notes'
+    : 'http://localhost:3001/notes';
+
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'main.js',
+    },
+    devServer: {
+      static: path.resolve(__dirname, 'build'),
+      compress: true,
+      port: 3000,
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
         },
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-react', '@babel/preset-env'],
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+          },
         },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        BACKEND_URL: JSON.stringify(backend_url),
+      }),
     ],
-  },
+  };
 };
 
 module.exports = config;
