27ebb9996a0685c2da45f7ca8f31207278c7a64b
Eric Chin <ericychin94@gmail.com>
2022-12-10 22:03:52 -0800

    User login

--- 08. GraphQL/phonebook-frontend/src/App.js
+++ 08. GraphQL/phonebook-frontend/src/App.js
@@ -1,17 +1,26 @@
-import { useQuery } from '@apollo/client';
+import { useApolloClient, useQuery } from '@apollo/client';
 import { useState } from 'react';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
 import PhoneForm from './components/PhoneForm';
+import LoginForm from './components/LoginForm';
 import { ALL_PERSONS } from './queries';
 
 const App = () => {
+  const [token, setToken] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
   const result = useQuery(ALL_PERSONS);
+  const client = useApolloClient();
 
   if (result.loading) {
     return <div>loading...</div> 
   }
+
+  const logout = () => {
+    setToken(null);
+    localStorage.clear();
+    client.resetStore();
+  };
 
   const notify = (message) => {
     setErrorMessage(message);
@@ -20,9 +29,23 @@ const App = () => {
     }, 10000);
   };
 
+  if (!token) {
+    return (
+      <div>
+        <Notify errorMessage={errorMessage} />
+        <h2>Login</h2>
+        <LoginForm
+          setToken={setToken}
+          setError={notify}
+        />
+      </div>
+    );
+  }
+
   return (
     <div>
       <Notify errorMessage={errorMessage} />
+      <button onClick={logout}>Logout</button>
       <Persons persons={result.data.allPersons} />
       <PersonForm setError={notify} />
       <PhoneForm setError={notify} />
