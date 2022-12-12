51ffe6d5854147438a25dfc3946471bc172b4b7f
Eric Chin <ericychin94@gmail.com>
2022-12-11 19:51:40 -0800

    Creating your first own types

--- 09. TypeScript/typescript-part9b/multiplier.ts
+++ 09. TypeScript/typescript-part9b/multiplier.ts
@@ -1,5 +1,29 @@
-const multiplicator = (a: number, b: number, printText: string) => {
-  console.log(printText, a * b);
+type Operation = 'multiply' | 'add' | 'divide';
+
+type Result = number;
+
+const calculator = (a: number, b: number, op: Operation): Result => {
+  switch (op) {
+    case 'multiply':
+      return a * b;
+    case 'divide':
+      if (b === 0) throw new Error('Can\'t divide by 0!');
+      return a / b;
+    case 'add':
+      return a + b;
+    default:
+      throw new Error('Operation is not multiply, add or divide!');
+  };
 };
 
-multiplicator('how about a string?', 4, 'Multiplied a string and 4, the result is:');
+console.log(process.argv);
+
+try {
+  console.log(calculator(1, 5, 'divide'));
+} catch (error: unknown) {
+  let errorMessage: string = 'Something went wrong';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+};
