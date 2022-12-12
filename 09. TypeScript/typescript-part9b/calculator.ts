16fcc1d58909456aa698f1fb77183e0aff4ca415
Eric Chin <ericychin94@gmail.com>
2022-12-11 23:25:32 -0800

    Renamed file to calculator and moved back multiplier to multiply

---
+++ 09. TypeScript/typescript-part9b/calculator.ts
@@ -0,0 +1,29 @@
+type Operation = 'multiply' | 'add' | 'divide';
+
+type Result = number;
+
+const calculator = (a: number, b: number, op: Operation): Result => {
+  switch (op) {
+    case 'multiply':
+      return a * b;
+    case 'divide':
+      if (b === 0) throw new Error('Can\'t divide by 0!');
+      return a / b;
+    case 'add':
+      return a + b;
+    default:
+      throw new Error('Operation is not multiply, add or divide!');
+  };
+};
+
+console.log(process.argv);
+
+try {
+  console.log(calculator(1, 5, 'divide'));
+} catch (error: unknown) {
+  let errorMessage: string = 'Something went wrong';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+};
--- 09. TypeScript/typescript-part9b/multiplier.ts
+++ 09. TypeScript/typescript-part9b/multiplier.ts
@@ -1,29 +1,5 @@
-type Operation = 'multiply' | 'add' | 'divide';
+const multiplicator = (a: number, b: number, printText: string) => {
+  console.log(printText,  a * b);
+}
 
-type Result = number;
-
-const calculator = (a: number, b: number, op: Operation): Result => {
-  switch (op) {
-    case 'multiply':
-      return a * b;
-    case 'divide':
-      if (b === 0) throw new Error('Can\'t divide by 0!');
-      return a / b;
-    case 'add':
-      return a + b;
-    default:
-      throw new Error('Operation is not multiply, add or divide!');
-  };
-};
-
-console.log(process.argv);
-
-try {
-  console.log(calculator(1, 5, 'divide'));
-} catch (error: unknown) {
-  let errorMessage: string = 'Something went wrong';
-  if (error instanceof Error) {
-    errorMessage += ' Error: ' + error.message;
-  }
-  console.log(errorMessage);
-};
+multiplicator(4, 5, 'Multiplied a 4 and 5, the result is:');
