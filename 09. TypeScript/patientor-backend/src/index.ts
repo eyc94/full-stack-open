8b8339f4955433edd6bf2ca09cd909f3fce1f200
Eric Chin <ericychin94@gmail.com>
2022-12-13 03:15:07 -0800

    Exercise 9.10

--- 09. TypeScript/patientor-backend/src/index.ts
+++ 09. TypeScript/patientor-backend/src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import diagnosesRouter from './routes/diagnoses';
+
 const app = express();
 app.use(express.json());
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
@@ -10,6 +12,8 @@ const PORT = 3001;
 app.get('/api/ping', (_req, res) => {
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
