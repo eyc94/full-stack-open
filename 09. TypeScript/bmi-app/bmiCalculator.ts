3ea8f0545b607c8fd4683a8ba8ba2b37241940bf
Eric Chin <ericychin94@gmail.com>
2022-12-12 16:36:50 -0800

    Exercise 9.5

--- 09. TypeScript/bmi-app/bmiCalculator.ts
+++ 09. TypeScript/bmi-app/bmiCalculator.ts
@@ -1,4 +1,4 @@
-const calculateBmi = (height: number, weight: number): string => {
+export const calculateBmi = (height: number, weight: number): string => {
   const heightInMeters = height / 100.0;
   const heightSquared = heightInMeters * heightInMeters;
   const bodyMassIndex = weight / heightSquared;
@@ -25,23 +25,23 @@ const calculateBmi = (height: number, weight: number): string => {
   return classification;
 };
 
-try {
-  if (process.argv.length < 3) {
-    throw new Error('Missing height and weight');
-  } else if (process.argv.length < 4) {
-    throw new Error('Missing weight');
-  } else if (process.argv.length > 4) {
-    throw new Error('Too many arguments');
-  }
-  const height: number = Number(process.argv[2]);
-  const weight: number = Number(process.argv[3]);
-  console.log(calculateBmi(height, weight));
-} catch (error: unknown) {
-  let errorMessage: string = 'Something went wrong.';
-  if (error instanceof Error) {
-    errorMessage += ' Error: ' + error.message;
-  }
-  console.log(errorMessage);
-};
+// try {
+//   if (process.argv.length < 3) {
+//     throw new Error('Missing height and weight');
+//   } else if (process.argv.length < 4) {
+//     throw new Error('Missing weight');
+//   } else if (process.argv.length > 4) {
+//     throw new Error('Too many arguments');
+//   }
+//   const height: number = Number(process.argv[2]);
+//   const weight: number = Number(process.argv[3]);
+//   console.log(calculateBmi(height, weight));
+// } catch (error: unknown) {
+//   let errorMessage: string = 'Something went wrong.';
+//   if (error instanceof Error) {
+//     errorMessage += ' Error: ' + error.message;
+//   }
+//   console.log(errorMessage);
+// };
 
 // console.log(calculateBmi(180.34, 77.1107));
--- 09. TypeScript/bmi-app/index.ts
+++ 09. TypeScript/bmi-app/index.ts
@@ -1,8 +1,28 @@
 import express from 'express';
+import { calculateBmi } from './bmiCalculator';
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+  const { height, weight } = req.query;
+  const isValidParameters: boolean = !isNaN(Number(height)) && !isNaN(Number(weight));
+
+  const bmi = calculateBmi(Number(height), Number(weight));
+
+  if (!height || !weight || !isValidParameters) {
+    res.status(400).send({
+      error: 'Malformatted parameters',
+    });
+  }
+
+  res.send({
+    weight: weight,
+    height: height,
+    bmi: bmi,
+  });
 });
 
 const PORT = 3003;
